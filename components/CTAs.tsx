04e6f73751935b8f7d007dcea86a78bef1732766
Travis Ueki <travueki@gmail.com>
2022-08-08 22:24:17 -0500

    fix ctas

--- components/CTAs.tsx
+++ components/CTAs.tsx
@@ -2,7 +2,7 @@ export const CTAs = () => {
   return (
     <div className="flex justify-center">
       <a href="https://housecallpro.com/book/Royalty-Carpet--Tile-Cleaning/331682fdd7e241ecb6a8ac97c3680572">
-        <button className="btn w-full">Book Online Now</button>
+        <button className="btn w-full mt-4">Book Online Now</button>
       </a>
     </div>
   );
